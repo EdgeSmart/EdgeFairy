6a70a1238a81abfdab42e934c8c9251d710e225b
Peter <houpeng1234@126.com>
2018-11-06 21:23:36 +0800

    Modify the run type

--- run/run.go
+++ run/run.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 
 	MQTT "github.com/eclipse/paho.mqtt.golang"
+	"github.com/vmihailenco/msgpack"
 )
 
 type topicItem struct {
@@ -23,12 +24,23 @@ type ProxyStruct struct {
 	Data          []byte
 }
 
+type Proxy struct {
+	ResponseTopic string
+	Method        string
+	URL           string
+	Proto         string
+	Host          string
+	Header        http.Header
+	Body          []byte
+}
+
 var (
 	mqttClient MQTT.Client
 	clientID   string
 	mqttServe  chan os.Signal
 	topicList  = []topicItem{
 		topicItem{topic: "%s/DockerAPI", qos: 0, handler: dockerAPIHandler},
+		topicItem{topic: "%s/DockerAPI_test", qos: 0, handler: dockerAPIHandlerTest},
 		topicItem{topic: "gateway_broadcast", qos: 0, handler: broadcastHandler},
 	}
 )
@@ -75,6 +87,64 @@ func defaultHandler(client MQTT.Client, msg MQTT.Message) {
 	fmt.Printf("MSG: %s\n", msg.Payload())
 }
 
+func dockerAPIHandlerTest(client MQTT.Client, msg MQTT.Message) {
+	var proxyData Proxy
+	proxyDataBytes := msg.Payload()
+	msgpack.Unmarshal(proxyDataBytes, &proxyData)
+
+	var requestBuf bytes.Buffer
+
+	// 拼接请求
+	WriteBytes(&requestBuf, []byte(fmt.Sprintf("%s %s %s\r\n", proxyData.Method, proxyData.URL, proxyData.Proto)))
+
+	WriteBytes(&requestBuf, []byte(fmt.Sprintf("Host: %s\r\n", proxyData.Host)))
+	// 处理header
+	for key := range proxyData.Header {
+		for i := range proxyData.Header[key] {
+			WriteBytes(&requestBuf, []byte(fmt.Sprintf("%s: %s\r\n", key, proxyData.Header[key][i])))
+		}
+	}
+
+	WriteBytes(&requestBuf, []byte("\r\n"))
+
+	WriteBytes(&requestBuf, []byte(proxyData.Body))
+	requestBytes := requestBuf.Bytes()
+
+	addr, err := net.ResolveUnixAddr("unix", "/var/run/docker.sock")
+	if err != nil {
+		panic("Cannot resolve unix addr: " + err.Error())
+	}
+	c, err := net.DialUnix("unix", nil, addr)
+	if err != nil {
+		panic("DialUnix failed.")
+	}
+
+	_, err = c.Write(requestBytes)
+	if err != nil {
+		panic("Writes failed.")
+	}
+
+	resFuf := make([]byte, 5120)
+	realLen, err := c.Read(resFuf)
+	realData := resFuf[0:realLen]
+	if err != nil {
+		panic("Read: " + err.Error())
+	}
+	fmt.Println(string(realData))
+	token := mqttClient.Publish(proxyData.ResponseTopic, 0, false, realData)
+	token.Wait()
+
+}
+
+func WriteBytes(buf *bytes.Buffer, c []byte) (int, error) {
+	len := 0
+	for i := range c {
+		buf.WriteByte(c[i])
+		len++
+	}
+	return len, nil
+}
+
 func dockerAPIHandler(client MQTT.Client, msg MQTT.Message) {
 	// var GolngGob bytes.Buffer
 	payload := bytes.NewReader(msg.Payload())
@@ -94,6 +164,8 @@ func dockerAPIHandler(client MQTT.Client, msg MQTT.Message) {
 	if err != nil {
 		panic("DialUnix failed.")
 	}
+	fmt.Println(proxyData.Data)
+	fmt.Println(string(proxyData.Data))
 
 	_, err = c.Write(proxyData.Data)
 	if err != nil {
